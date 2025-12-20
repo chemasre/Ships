bbb6913a2b4bb289b49302a8969df1d88397d312
jsolisifp <chema.sre@gmail.com>
2025-12-20 13:46:49 +0100

    Added balloons parallax object

--- scripts/parallaxdata.js
+++ scripts/parallaxdata.js
@@ -1,14 +1,14 @@
-    var parallaxNumObjects = [3, 2];
+    var parallaxNumObjects = [3, 3];
     var parallaxNumPlanes = 2;
     
     var _fair = 0;
 
-    var parallaxObjectsTheme        = [ [ _fair, _fair, _fair], [ _fair, _fair] ];
-    var parallaxObjectsWidth        = [ [   619,   241,   189], [   343,   848] ];
-    var parallaxObjectsHeight       = [ [   706,   324,   162], [   248,   297] ];
-    var parallaxObjectsCenterX      = [ [   293,   108,    85], [   168,   414] ];
-    var parallaxObjectsCenterY      = [ [   428,   206,   111], [   193,   239] ];
-    var parallaxObjectsYMin         = [ [   -50,   124,   200], [    85,    25] ];
-    var parallaxObjectsYMax         = [ [    70,   246,   256], [    85,    40] ];
-    var parallaxObjectsRotationMin  = [ [     0,     0,     0], [     0,     0] ];
-    var parallaxObjectsRotationMax  = [ [   360,   360,   360], [     0,     0] ];
+    var parallaxObjectsTheme        = [ [ _fair, _fair, _fair], [ _fair, _fair, _fair] ];
+    var parallaxObjectsWidth        = [ [   619,   241,   189], [   343,   848,   594] ];
+    var parallaxObjectsHeight       = [ [   706,   324,   162], [   248,   297,   506] ];
+    var parallaxObjectsCenterX      = [ [   293,   108,    85], [   168,   414,   301] ];
+    var parallaxObjectsCenterY      = [ [   428,   206,   111], [   193,   239,   399] ];
+    var parallaxObjectsYMin         = [ [   -50,   124,   200], [    85,    25,  -177] ];
+    var parallaxObjectsYMax         = [ [    70,   246,   256], [    85,    40,  -177] ];
+    var parallaxObjectsRotationMin  = [ [     0,     0,     0], [     0,     0,     0] ];
+    var parallaxObjectsRotationMax  = [ [   360,   360,   360], [     0,     0,     0] ];
